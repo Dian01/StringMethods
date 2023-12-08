2b22620b6cf00d26af0178f503fd92fb06f07b22
Dian01 <33253205+Dian01@users.noreply.github.com>
2023-12-08 18:28:35 +0100

    Test Commit

--- StringMethods/Program.cs
+++ StringMethods/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is another test!");
+            Console.WriteLine("A new line");
         }
     }
 }
